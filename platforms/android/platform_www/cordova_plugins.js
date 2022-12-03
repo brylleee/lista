b99aabec626017ed53cac9733816871c927228f3
kairo <itokairo.zc@gmail.com>
2022-12-03 16:49:59 +0800

    Clean up

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-android-permissions.Permissions",
+      "file": "plugins/cordova-plugin-android-permissions/www/permissions.js",
+      "pluginId": "cordova-plugin-android-permissions",
+      "clobbers": [
+        "cordova.plugins.permissions"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-android-permissions": "1.1.5"
+  };
+});
